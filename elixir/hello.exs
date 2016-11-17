9e7c0427b918edeea55340d42163a7efa807cceb
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2016-11-18 07:46:04 +0900

    zephir array

--- elixir/hello.exs
+++ elixir/hello.exs
@@ -1,9 +1,6 @@
 IO.puts "Hello, World!"
-for {a, b} <- [{1,2},{3,4},{5,6}] do
-  IO.puts a
-  IO.puts b
+for char <- 'Ichikawa' do
+  IO.puts char
 end
 
-{status, file} = File.open("hello.exs")
-IO.puts status
 
